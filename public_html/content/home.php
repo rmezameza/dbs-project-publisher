0973a68c9831b5690a4ca6d7a0beb38828c65abd
Roberto Alfredo Meza Meza <mezamezar87@univie.ac.at>
2021-12-28 20:13:20 +0100

    Designed homepage

--- public_html/content/home.php
+++ public_html/content/home.php
@@ -4,7 +4,28 @@
         <title>Bahoe Books - Home</title>
     </head>
     <body>
-        <h1>Home</h1>
+        <h1 class="h1">Dashboard</h1>
+            <div class="container mt-5 border border-2 border-dark rounded rounded-3">
+                <h2 class="h2 mt-3">Die drei neuesten Bücher:</h2>
+                <!-- This row shows the three newest / latest inserted books -->
+                <div class="row mb-5">
+                    <div class="col">Column 1</div>
+                    <div class="col">Column 2</div>
+                    <div class="col">Column 3</div>
+                </div>
+                    <!-- TODO: Change color of horizontal line -->
+                <hr class="text-decoration-line-through">
+                <h2 class="h2 mt-5">Status der Buchlager:</h2>
+                <!-- This row shows the status of the total capacity and amount of books of all warehouses. -->
+                <!-- And it shows the capacity and number of books in every warehouse -->
+                <div class="row">
+                    <div class="col">Status der gesamten Buchlager:</div>
+                </div>
+                    <!-- TODO: I want to insert sql rows for every book warehouse -->
+                <div class="row mb-3">
+                    <div class="col">Status von </div>
+                </div>
+            </div>
     </body>
 </html>
 <?php
